f475bfac9316fb530776312f89bddb85b3cbec39
LuisCasto <axel_casti@hotmail.com>
2026-03-11 14:47:43 -0600

    fix: switch to asyncpg driver

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from app.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 
-DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://")
+DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
