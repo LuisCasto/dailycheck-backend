83e5dc0c32c900a3d4b1b58c0e6e15cc53d29c4d
LuisCasto <axel_casti@hotmail.com>
2026-03-11 14:39:59 -0600

    fix: explicit psycopg2 driver in database URL

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(settings.DATABASE_URL)
+DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://")
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
